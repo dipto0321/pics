f6265738fdfdac98221edcd9a7bcddd0abb0e761
Dipto Karmakar <diptokmk47@gmail.com>
2019-07-01 18:28:01 +0600

    Initially prepare search form
    - Connect App with Search

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import "./app.scss";
-export default function App() {
-  return (
-    <div className="ui container top-margin">
-      <SearchBar />
-    </div>
-  );
+export default class App extends Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+  render() {
+    return (
+      <div className="ui container top-margin">
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+  state = { term: "" };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
   render() {
     return (
       <div className="ui segment">
         <div className="ui form">
           <div className="field">
-            <form action="">
+            <form onSubmit={this.onFormSubmit}>
               <label htmlFor="searchbar">Image Search</label>
-              <input type="text" name="searchbar" id="searchbar" />
+              <input
+                type="text"
+                name="searchbar"
+                id="searchbar"
+                value={this.state.term}
+                onChange={e => this.setState({ term: e.target.value })}
+              />
             </form>
           </div>
         </div>
