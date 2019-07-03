7d707663434e570bc53f776460b9378d2d21d239
Dipto Karmakar <diptokmk47@gmail.com>
2019-07-04 00:33:32 +0600

    Request a get using axios
    - Add axios in package.
    - Add that package to `App.js`

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 import "./app.scss";
 export default class App extends Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization:
+          "Client-ID bb4a5602f82d0ce92894bff097bdaf2046fb37348f1650d9d86ece5df7ad4bbb"
+      }
+    });
   }
   render() {
     return (
